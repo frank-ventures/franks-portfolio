aa8c65c11d35bbf9ecccadb915b56ac64f3d13c3
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-07-16 21:02:02 +0100

    add use-sound library to enable audio

--- src/components/header/HeroIntro.tsx
+++ src/components/header/HeroIntro.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { ReactNode, useState, useEffect } from "react";
+import { ReactNode } from "react";
 import PlayIcon from "../icons/techIcons/PlayIcon";
+import useSound from "use-sound";
 
 export default function HeroIntro({
   text,
@@ -9,28 +10,7 @@ export default function HeroIntro({
   text: ReactNode;
   audio?: string;
 }) {
-  const [thisAudio] = useState(new Audio(audio)) || "";
-  const [playing, setPlaying] = useState(false);
-
-  function toggle() {
-    setPlaying(!playing);
-  }
-
-  useEffect(() => {
-    if (playing) {
-      thisAudio.play();
-    } else {
-      thisAudio.pause();
-      thisAudio.currentTime = 0;
-    }
-  }, [playing]);
-
-  useEffect(() => {
-    thisAudio.addEventListener("ended", () => setPlaying(false));
-    return () => {
-      thisAudio.removeEventListener("ended", () => setPlaying(false));
-    };
-  }, []);
+  const [play] = useSound(audio || "");
 
   return (
     <div className="bg-red-600 pt-16 border-2 border-black p-10 w-full">
@@ -39,14 +19,7 @@ export default function HeroIntro({
       </h1>
       {audio && (
         <section className="absolute z-20 top-16 left-1/2 -translate-x-52 bg-yellow-400 rounded-3xl shadow-md shadow-black hover:cursor-pointer hover:bg-blue-400 hover:scale-125 duration-300">
-          <i
-            className=""
-            onClick={() => {
-              toggle();
-            }}
-          >
-            {PlayIcon(40, 40)}
-          </i>
+          <i onClick={() => play()}>{PlayIcon(40, 40)}</i>
         </section>
       )}
     </div>
